c137761886675e8f4b9cdc08cd94ca05a94bddcb
matthcol <matthias.colin@gmail.com>
2026-07-06 05:52:11 +0200

    finalizing movie base project

---
+++ projet_movie_base/app_movie.py
@@ -0,0 +1,44 @@
+from movie import Movie
+
+
+# m = Movie()
+m1 = Movie("La bataille de Gaulle: L'âge de fer", 2026, 160)
+m2 = Movie(title='Les Tuche 4', year=2021)
+
+movies = [m1, m2]
+
+for m in movies:
+    print(m) # str
+    print(str(m))
+    print(f"Hier, j'ai vu {m}") # str
+    print(repr(m))
+    print('title:', m.title)
+    print('year:', m.year)
+    print('duration:', m.duration if m.duration is not None else 'NA')
+    print()
+
+print('Longueur:', len(m2))
+m2.duration = 101 # '1h41'
+print(m2)
+print('Longueur:', len(m2))
+
+
+for m in movies:
+    if 'tuche' in m:
+        print('[Found]', m)
+    # else:
+    #     print('[Skip]', m)
+
+print(2021 in m2)
+print(m2 == 'un ventilateur')
+
+h_m = m2.duration_hour_minute()
+if h_m is not None:
+    h, m = h_m
+    print(f"{h}h{m}mn")
+
+
+hm_list = [m.duration_hour_minute() for m in movies if m.duration]
+print(hm_list)
+
+# movies.duration_hour_minute() # unknown method
---
+++ projet_movie_base/movie.py
@@ -0,0 +1,39 @@
+# module movie
+
+from dataclasses import dataclass
+
+
+@dataclass # stdlib Python (3.8+), alt pydantic
+class Movie:
+    """ Represents a movie with its title, release year and duration in minutes
+
+    Example:
+
+        m = Movie("La bataille de Gaulle: L'âge de fer", 2026, 160)
+    """
+
+    title: str
+    year: int
+    duration: int | None = None
+
+    # implements builtin functions 
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.year}, {self.duration}mn)"
+    
+    def __len__(self) -> int:
+        return self.duration if self.duration is not None else 0
+    
+    # surcharge d'opérateur
+
+    # opérateur in
+    def __contains__(self, item) -> bool:
+        if isinstance(item, str):
+            return item.lower() in self.title.lower()
+        else:
+            return False
+        
+    def duration_hour_minute(self) -> tuple[int, int] | None:
+        if self.duration is not None:
+            return divmod(self.duration, 60)
+        # implicit return None
